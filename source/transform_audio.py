e125ccbf8f8935998c478558555096d4efa873e5
Jonathan Woodard <jwoodard@gmail.com>
2015-09-15 11:21:41 -0700

    refine functions to translate mp3 to wav

--- source/transform_audio.py
+++ source/transform_audio.py
@@ -22,24 +22,61 @@ def make_dirs(audio_path):
 
 def dir_list(audio_path):
     '''
-    generate a list of all directories in the audio path
+    INPUT:
+        path to data file directories
+    OUTPUT:
+        For each directory containing mp3 files, generate a new directory
+        to recieve wav files.  Return a list of tuples containing mp3 and wav
+        directory paths.
     '''
-    d_list = []
+    directory_list = []
     for (_, _, filename) in os.walk(audio_path + "json_files"):
         for name in filename:
-            d_list.append(audio_path + name + "/")
-    return d_list
+            input_directory = audio_path + name
+            output_directory = input_directory + "_wav"
+            directory_list.append((input_directory, output_directory))
+    return directory_list
 
 
-def file_list(dir_list):
+def make_file_list(directory_list):
     '''
-    generate a list of files in each directory
+    INPUT:
+        list of tuples containing input, output directories
+    OUTPUT:
+        list of tuples containing input, output file names
     '''
-    f_list = []
-    for (_, _, filename) in os.walk(dir_list):
-        for f_id in filename:
-            f_list.append(mp3_directory + "/" + f_id)
-    return f_list
+    file_list = []
+    for directory in directory_list:
+        for (_, _, filenames) in os.walk(directory[0]):
+            for file_id in filenames:
+                mp3_file = (directory[0] + "/" + file_id)
+                wav_file = (directory[1] + "/" + file_id[:-3] + "wav")
+                file_list.append((mp3_file, wav_file))
+    return file_list
+
+
+def make_wav(file_list):
+    '''
+    INPUT:
+        list of tuples containing input, output file names
+    OUTPUT:
+        wav files
+    '''
+    while len(file_list) != 0:
+        f = file_list.pop()
+        sound = AudioSegment.from_mp3(f[0])
+        sound.export(f[1])
+
+
+def make_fft(wav_directory, csv_directory):
+    for (_, _, filenames) in os.walk(wav_directory):
+        for file_id in filenames:
+            a = read(file_id)
+            nparray = np.array(a[1], dtype=float)
+            f = np.fft.fft(nparray)
+            np.savetxt(csv_directory + "/" + file_id[:-3] + ".csv", f, delimiter=",")
+
+
 
     wav_directory = mp3_directory + "_wav"
     csv_directory = mp3_directory + "_csv"
